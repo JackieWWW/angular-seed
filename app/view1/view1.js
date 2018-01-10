8d857068d008fbda712739c1418bf5f9544663a6
jackie <jackie.j.www@gmail.com>
2018-01-10 14:19:18 -0500

    Finish basic front-end functionalities

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,52 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+  $scope.user = {};
+  $scope.list = [];
+  $scope.mainModel = {
+       value1 : false,
+       value2 : false,
+       value3 : false,
+       value4 : false
+  };
+  $scope.drinkModel = {
+       value1 : false,
+       value2 : false,
+       value3 : false,
+       value4 : false,
+       value5 : false
+  };
+  $scope.fruitModel = {
+       value1 : false,
+       value2 : false,
+       value3 : false,
+       value4 : false
+  };
+  $scope.pick = (model) => {
+    var result = [];
+    for(var key in $scope[model]){
+      if($scope[model][key]) result.push($scope[model][key]);
+    }
+    $scope.user[model] = result.join(', ')
+    return $scope.user[model];
+  };
+  $scope.clear = (index) => {
+    // const index = $scope.list.indexOf(username);
+    $scope.list.splice(index, 1);
+  };
+  $scope.update = (username, index) => {
+    $scope.user.name = username;
+    $scope.clear(index);
+  };
+  $scope.reset = () => {
+    $scope.list.push($scope.user);
+    const models = ['mainModel', 'drinkModel', 'fruitModel'];
+    models.forEach((model) => {
+      for(var key in $scope[model]){
+        $scope[model][key] = false;
+      }
+    });
+    $scope.user = {};
+  };
 }]);
